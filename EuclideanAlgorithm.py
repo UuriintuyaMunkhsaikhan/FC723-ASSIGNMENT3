d7cd25549289d9ad723710d6e1034ebff1a96fd3
monkh <monkhsaihanuuriituya@gmail.com>
2024-04-14 21:05:11 +0100

    Pseudocode for the Euclidean Algorithm

--- EuclideanAlgorithm.py
+++ EuclideanAlgorithm.py
@@ -1,28 +1,9 @@
-def Euclidean_algorithm(a, b):
-    while b != 0:
-        temp = b
-        b = a % b
-        a = temp
-    return a
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+class Euclidean_algorithm:
+    @staticmethod
+    def gcd(a, b):
+        while b != 0:
+            temp = b
+            b = a % b
+            a = temp
+        return a
 
